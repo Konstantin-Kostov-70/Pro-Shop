164246a8717e8d812fb64951554e624fe0df2057
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-06-25 15:48:52 +0200

    Create components Product,Rating and HomePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Container } from "react-bootstrap";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome</h1>
+          <HomePage />
         </Container>
       </main>
       <Footer />
