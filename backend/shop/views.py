434c9d6d2490b2a0be1679918530e402314b9403
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-06-26 03:45:13 +0200

    install django rest  and add to installed apps in settings

--- backend/shop/views.py
+++ backend/shop/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .products import products
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def get_routes(request):
+    routes = [
+        "/api/products/",
+        "/api/products/create/",
+        "/api/products/upload/",
+        "/api/products/<id>/reviews/",
+        "/api/products/top/",
+        "/api/products/<id>/",
+        "/api/products/delete/<id>/",
+        "/api/products/update/<id>",
+    ]
+    return Response(routes)
+
+
+@api_view(['GET'])
+def get_products(request):
+    return Response(products)
+
+
+@api_view(['GET'])
+def get_product(request, pk):
+    product = next((p for p in products if p['_id'] == pk), None)
+    return Response(product)
+
+  
