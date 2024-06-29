915c2f4a2cb1f0c691a4a1d0d4c5938ca13b54e4
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-06-29 04:22:16 +0200

    add cart and product reducers to root reducer and add local store to redux store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,15 +1,27 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { thunk } from 'redux-thunk';
-import { productListReducer } from './reducers/productRedusers'
+import { productListReducer, productDetailReducer } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
 
 const rootReducer = combineReducers({
-  productList:productListReducer,
+  productList: productListReducer,
+  productDetails: productDetailReducer,
+  cart: cartReducer,
 });
 
+const cartItemsFromStorage = localStorage.getItem('cartItems') ?
+   JSON.parse(localStorage.getItem('cartItems')) : []
+
+const initialState = {
+  cart: {
+    cartItems: cartItemsFromStorage
+  }
+}
 
 const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+  preloadedState: initialState,
   devTools: process.env.NODE_ENV !== 'production', 
 });
 
