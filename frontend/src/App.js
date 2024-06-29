a00d067a43ad6085d59ef76ef0fd869098daa5e3
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-06-29 04:20:15 +0200

    add router to cart page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/product/:id" element={<ProductPage />} />
+            <Route path="cart/:id?" element={<CartPage />} />
           </Routes>
         </Container>
       </main>
