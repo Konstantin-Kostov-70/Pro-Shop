789b39b3f3c0420f42d03b92032ca787ce425e44
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-06-29 22:14:24 +0200

    create urls

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.get_routes, name='routes'),
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='my_token_obtain_pair'),
+    path('users/register/', views.register_user, name='register'),
+    path('users/', views.get_users, name='users'),
+    path('users/profile/', views.get_user_profile, name='user-profile'),
     path('products/', views.get_products, name='products'),
     path('products/<str:pk>/', views.get_product, name='product'),
 ]
