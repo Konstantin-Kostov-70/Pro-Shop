ab09cc359bd5b43386e6af4d5f5bcd933392b144
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-07-01 00:22:26 +0200

    add user login and register reducers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,19 +2,28 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { thunk } from 'redux-thunk';
 import { productListReducer, productDetailReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
 
 const rootReducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
    JSON.parse(localStorage.getItem('cartItems')) : []
 
+   const userInfoFromStorage = localStorage.getItem('userInfo') ?
+   JSON.parse(localStorage.getItem('userInfo')) : null
+
 const initialState = {
   cart: {
     cartItems: cartItemsFromStorage
+  },
+  userLogin: { 
+     userInfo: userInfoFromStorage
   }
 }
 
