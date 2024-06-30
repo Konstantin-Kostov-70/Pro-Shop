973e67a5008e752029dde758e80e998f683bf4c8
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-07-01 00:26:55 +0200

    create profile and logout links in dropdown menu and logout functionality

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react'
-import { Container, Navbar, Nav, } from 'react-bootstrap'
+import { useSelector, useDispatch } from 'react-redux'
+import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import { logout } from '../actions/userActions'
 
 function Header() {
+
+  const userLogin = useSelector(state => state.userLogin);
+  const { userInfo } = userLogin
+
+  const dispatch = useDispatch()
+
+  const logoutHandler = () => {
+   dispatch(logout());
+  }
+
   return (
     <header>
      <Navbar bg="dark" data-bs-theme="dark">
@@ -14,9 +26,18 @@ function Header() {
             <LinkContainer to="/cart">
               <Nav.Link><i className='fas fa-shopping-cart'></i><span className='nav-span'>Cart</span></Nav.Link>
             </LinkContainer>
-            <LinkContainer to="/login">
-              <Nav.Link><i className='fas fa-user'></i><span className='nav-span'>Login</span></Nav.Link>
-            </LinkContainer>
+            {userInfo ? (
+              <NavDropdown title={userInfo.name} id='username'>
+                <LinkContainer to={'/profile'}>
+                   <NavDropdown.Item>Profile</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <LinkContainer to="/login">
+                <Nav.Link><i className='fas fa-user'></i><span className='nav-span'>Login</span></Nav.Link>
+              </LinkContainer>
+            )}
           </Nav>
         </Container>
       </Navbar>
