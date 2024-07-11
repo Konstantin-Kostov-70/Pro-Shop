ac23bfd2f3e94a116663c0dcccf9533ed89060af
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-07-11 20:43:47 +0200

    create get-user, update user views and urls for them

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -7,9 +7,11 @@ urlpatterns = [
         path('', views.get_users, name='users'),
         path('login/', views.MyTokenObtainPairView.as_view(), name='my-token-obtain-pair'),
         path('register/', views.register_user, name='register'),
+        path('update/<str:pk>/', views.update_user, name='update-user'),
         path('delete/<str:pk>/', views.delete_user, name='delete-user'),
         path('profile/', views.get_user_profile, name='user-profile'),
         path('profile/update/', views.update_user_profile, name='update-profile'),
+        path('<str:pk>/', views.get_user_by_id, name='get-user'),
     ])),
 
     path('products/', include([
