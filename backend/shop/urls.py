0364319e0d0547fe2a907739d3035196647c12e3
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-07-13 11:48:20 +0200

    Created create,update and delete product views , and urls for them

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -16,7 +16,10 @@ urlpatterns = [
 
     path('products/', include([
         path('', views.get_products, name='products'),
+        path('create/', views.create_product, name='create-product'),
         path('<str:pk>/', views.get_product, name='product'),
+        path('update/<str:pk>/', views.update_product, name='update-product'),
+        path('delete/<str:pk>/', views.delete_product, name='delete-product'),
     ])),
     
     path('orders/', include([
--- backend/shop/views.py
+++ backend/shop/views.py
@@ -130,6 +130,51 @@ def get_product(request, pk):
 
 
 @api_view(['POST'])
+@permission_classes([IsAdminUser])
+def create_product(request):
+    user = request.user
+    product = Product.objects.create(
+        user = user,
+        name = 'Sample name',
+        price = 0,
+        brand = 'Sample brand',
+        countInStock = 0,
+        category = 'Sample Category', 
+        description = ''
+    )
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def update_product(request, pk):
+    data = request.data
+    product = Product.objects.get(_id=pk)
+
+    product.name = data['name']
+    product.price = data['price']
+    product.brand = data['brand']
+    product.countInStock = data['countInStock']
+    product.category = data['category']
+    product.description = data['description']
+
+    product.save()
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def delete_product(request, pk):
+    product = Product.objects.get(_id=pk)
+    product.delete()
+    return Response('Product was deleted')
+
+
+@api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def add_order_items(request):
     user = request.user
