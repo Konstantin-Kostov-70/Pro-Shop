ce07c3f717c8b79b7845c83c698b5573893b3275
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-07-13 21:46:38 +0200

    rename paidAt field in Order model

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -39,7 +39,7 @@ class Order(models.Model):
     shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank= True)
     totalPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank= True)
     isPaid = models.BooleanField(default=False)
-    payAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
     isDelivered = models.BooleanField(default=False)
     deliveredAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
     createdAt = models.DateTimeField(auto_now_add=True)
