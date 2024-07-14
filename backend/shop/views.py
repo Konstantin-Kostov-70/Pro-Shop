5cf0c932d4c38235ff6123ae7929b6179152b0f4
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-07-15 00:56:57 +0200

    update details to detail in create review view

--- backend/shop/views.py
+++ backend/shop/views.py
@@ -198,11 +198,11 @@ def create_product_review(request, pk):
     already_exist = product.review_set.filter(user=user).exists()
 
     if already_exist :
-        content = {'details': 'Product already reviewed'}
+        content = {'detail': 'Product already reviewed'}
         return Response(content, status=status.HTTP_400_BAD_REQUEST)
     
     elif data['rating'] == 0 :
-        content = {'details': 'Please select a rating'}
+        content = {'detail': 'Please select a rating'}
         return Response(content, status=status.HTTP_400_BAD_REQUEST)
     
     else :
