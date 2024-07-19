0da85d9fc9da226c90b212b829e0300d55ab8f71
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-07-19 22:09:12 +0200

    added className to Product.js

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -5,7 +5,7 @@ import Rating from "./Rating";
 
 function Product({ product }) {
   return (
-    <Card className="my-3 p-3">
+    <Card className="my-3 p-3 products-card">
       <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} />
       </Link>
